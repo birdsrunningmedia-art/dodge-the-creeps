f293041ffc613401f35b396ba99ce88cd1c383fa
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2026-06-21 05:41:30 +0100

    finished the 2d game

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,80 +1,67 @@
 extends Node
 
-# Scene used to create new mobs during gameplay.
 @export var mob_scene: PackedScene
-
-# Tracks the player's score.
 var score
 
-
-# Called once when this node enters the scene tree.
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
-
-# Called every frame.
-func _process(delta: float) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
 	pass
 
 
-# Called when the player emits the "hit" signal.
-# Stops the game by stopping the timers that create mobs and score points.
-func game_over() -> void:
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-
-
-# Resets the game state and starts a new round.
 func new_game():
 	score = 0
-
-	# Reset the player to the starting position and enable control.
 	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
-	# Start the delay before mobs begin spawning.
-	$ScoreTimer.start()
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
-# Called whenever the MobTimer times out.
-# Creates and launches a new mob from a random point on the spawn path.
 func _on_mob_timer_timeout():
-	# Create a new mob instance from the Mob scene.
+	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
 
-	# PathFollow2D used to pick a random spawn position on the Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-
-	# Move to a random point along the path.
+	# Choose a random location on Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 
-	# Place the mob at the selected position.
-	mob.position = mob_spawn_location.position
-
-	# Get the direction perpendicular to the path.
+	# Set the mob's direction perpendicular to the path direction.
 	var direction = mob_spawn_location.rotation + PI / 2
 
-	# Add some randomness so mobs spread out.
-	direction += randf_range(-PI / 4, PI / 4)
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
 
-	# Rotate the mob to face its movement direction.
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
 	mob.rotation = direction
 
-	# Create a random speed and convert it into a velocity vector.
+	# Choose the velocity for the mob.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
-	# Add the mob to the scene so it becomes active.
+	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
 
-# Called every second (or whatever interval ScoreTimer uses).
-# Increases the player's score over time.
-func _on_score_timer_timeout() -> void:
+func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
-# Called when the StartTimer finishes.
-# Begins spawning mobs and counting score.
-func _on_start_timer_timeout() -> void:
+func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	
